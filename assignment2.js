72e705f172e44723d9bbf20b3d3f92f0992c934d
David Nguyen <dav1d.nguyen@outlook.com>
2020-12-14 18:44:47 +1100

    Assignment 2 done

---
+++ assignment2.js
@@ -0,0 +1,18 @@
+let a = "Coder"
+
+let b = "School"
+
+console.log (a+b)
+
+// Operators //
+
+console.log (10 +24)
+console.log ("10"+"24")
+console.log ("Hello" + " " +"2021")
+console.log (1 + 2 * 3)
+console.log ((1+3) ** 2)
+console.log (1/0)
+console.log (6 % 2)
+console.log (5.5 % 2)
+console.log (Number("123"))
+console.log (typeof(1+""))
