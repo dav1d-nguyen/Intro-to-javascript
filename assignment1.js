8867bcc9bb67c94ae7f3ebc215659d306c4f5345
David Nguyen <dav1d.nguyen@outlook.com>
2020-12-14 18:39:09 +1100

    Assignment 1 done

---
+++ assignment1.js
@@ -0,0 +1,14 @@
+console.log ("Area of a circle")
+
+let pi = 3.14
+
+console.log (typeof(pi))
+
+let radius = 5
+
+console.log (typeof(radius))
+
+let area = pi * radius ** 2
+
+console.log (`The area of a circle given the radius ${radius} (m) is ${area} (m2).`)
+
