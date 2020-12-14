fc85dcbc06b3e61bd80fc7af647f281a9705fbea
David Nguyen <dav1d.nguyen@outlook.com>
2020-12-14 20:01:49 +1100

    Assignment 4 - only 2/3 done

---
+++ assignment4.js
@@ -0,0 +1,34 @@
+// Loop to order numbers
+
+let a = 9
+let b = 5
+
+if (a<b) {
+  for (a; a <= b; a++) {
+    console.log (a)
+  }
+} else {
+  for (a; a >= b; a--) {
+    console.log (a)
+  }
+}
+
+// Loop to find the sum of numbers
+
+let x = 9
+let y = 12
+
+var sum_list = []
+
+let total = 0
+
+for (x; y >= x; x++) {
+  total = total + x
+}
+
+console.log (total)
+
+// 4-digit sum of the digits
+
+
+
