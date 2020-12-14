934e14ecac89671cdd3553051ac7b90d087a4acd
David Nguyen <dav1d.nguyen@outlook.com>
2020-12-14 21:18:37 +1100

    Assignment 7 done

---
+++ assignment7.js
@@ -0,0 +1,11 @@
+// Random output between 0 and 2
+
+console.log(Math.floor(Math.random() * 3))
+
+// Random output between 0 and 1
+
+console.log(Math.floor(Math.random() * 2))
+
+// Random output between 0 and 10
+
+console.log(Math.floor(Math.random() * 11))
